548255e9cde321eee3315f721522bec20d142856
Joram van den Boezem <joram@vandenboezem.nl>
2022-03-21 15:53:36 +0000

    chore: theme

--- os/create_symbolic_links.sh
+++ os/create_symbolic_links.sh
@@ -35,12 +35,6 @@ create_symlinks() {
     local i=""
     local sourceFile=""
     local targetFile=""
-    local skipQuestions=false
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    skip_questions "$@" \
-        && skipQuestions=true
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -49,32 +43,27 @@ create_symlinks() {
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-        if [ ! -e "$targetFile" ] || $skipQuestions; then
+        if [ ! -e "$targetFile" ]; then
 
             execute \
                 "ln -fs $sourceFile $targetFile" \
                 "$targetFile → $sourceFile"
 
         elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            
             print_success "$targetFile → $sourceFile"
+        
         else
 
-            if ! $skipQuestions; then
+            mv "$targetFile" "$targetFile.old"
 
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
+            print_warning "$targetFile → $targetFile.old"
+            
+            rm -rf "$targetFile"
 
-                    rm -rf "$targetFile"
-
-                    execute \
-                        "ln -fs $sourceFile $targetFile" \
-                        "$targetFile → $sourceFile"
-
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
-
-            fi
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
 
         fi
 
