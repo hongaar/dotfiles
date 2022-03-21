16933eec65848f25acb1d1a978158154d08b7dff
Joram van den Boezem <joram@vandenboezem.nl>
2022-03-21 16:24:59 +0000

    chore: disable parts

--- os/installs/main.sh
+++ os/installs/main.sh
@@ -17,18 +17,17 @@ print_in_purple "\n • Installs\n\n"
 update
 upgrade
 
+./build-essentials.sh
 ./git.sh
-
-# ./build-essentials.sh
-#./nvm.sh
-#./browsers.sh
-#./compression_tools.sh
-#./image_tools.sh
-#./misc.sh
-#./misc_tools.sh
-#./npm.sh
-#./tmux.sh
-#./vim.sh
+./nvm.sh
+./browsers.sh
+./compression_tools.sh
+./image_tools.sh
+./misc.sh
+./misc_tools.sh
+./npm.sh
+./tmux.sh
+./vim.sh
 
 ./cleanup.sh
 
